fd8ff43e71cc95d5a7a26f004948eb0532ba80c8
Neuxs <Neuxsyt@gmail.com>
2024-03-19 21:33:38 -0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import webview as wv
-
-def main():
-    wv.create_window(title='Cosmos Launcher', url='win/index.html', width=800, height=600, resizable=False)
-    wv.start()
-
-if __name__ == "__main__":
-    main()
